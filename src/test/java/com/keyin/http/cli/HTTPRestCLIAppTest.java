21e19c633aeab50796a6627f6a8b23be4f95a22e
ashley-colbert <136830986+ashley-colbert@users.noreply.github.com>
2024-06-25 18:48:18 -0230

    mockito working

--- src/test/java/com/keyin/http/cli/HTTPRestCLIAppTest.java
+++ src/test/java/com/keyin/http/cli/HTTPRestCLIAppTest.java
@@ -1,4 +1,42 @@
 package com.keyin.http.cli;
 
+import com.keyin.domain.Airports;
+import com.keyin.http.cli.HTTPRestCLIApp;
+import com.keyin.http.client.RESTClient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 public class HTTPRestCLIAppTest {
+    @Mock
+    private RESTClient mockRESTClient;
+
+    private HTTPRestCLIApp httpRestCLIAppUnderTest;
+
+    @Test
+    public void testGenerateAirportsReport() {
+        HTTPRestCLIApp httpRestCLIApplicationUnderTest = new HTTPRestCLIApp();
+
+        Airports stJohnsAirport = new Airports();
+        stJohnsAirport.setCode("YYT");
+        stJohnsAirport.setName("St. John's Airport");
+        stJohnsAirport.setId("1");
+
+        List<Airports> airportList = new ArrayList<Airports>();
+        airportList.add(stJohnsAirport);
+
+        Mockito.when(mockRESTClient.getAllAirports()).thenReturn(airportList);
+
+        httpRestCLIApplicationUnderTest.setRestClient(mockRESTClient);
+
+        Assertions.assertTrue(httpRestCLIApplicationUnderTest.generateAirportReport().contains("YYT"));
+
+    }
 }
